069cf2233eb6584140c1427b5db039413de17e75
sabbir3109 <78914877+sabbir3109@users.noreply.github.com>
2021-04-23 04:52:26 +0600

    ...

--- themes/matheco/assets/js/main.js
+++ themes/matheco/assets/js/main.js
@@ -303,6 +303,52 @@ $('.filter-prdt').click(function(){
 }
 }
 
+/**
+Slick slider
+*/
+if( $('.hasRelatedProduct').length ){
+    $('.hasRelatedProduct').slick({
+      dots: false,
+      arrows:false,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 700,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
+
+
 /*start of Milon*/
 
 if( $('.TeamMemberSlider').length ){
