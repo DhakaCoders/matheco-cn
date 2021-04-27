c59f09e39e68439b160c4e89c03cba223498b860
haque <azizul.softsw@gmail.com>
2021-04-27 17:27:33 +0600

    Update class-cbv_wc_category_widgets.php

--- themes/matheco/inc/class-cbv_wc_category_widgets.php
+++ themes/matheco/inc/class-cbv_wc_category_widgets.php
@@ -21,6 +21,53 @@ class cbv_wc_category_widget extends WC_Widget {
 		$this->widget_description = __( 'Display a list of categories to filter products in your store.', 'woocommerce' );
 		$this->widget_id          = 'woocommerce_layered_category_nav';
 		$this->widget_name        = __( 'CBV Filter Products by Category', 'woocommerce' );
+		$this->settings           = array(
+			'title'              => array(
+				'type'  => 'text',
+				'std'   => __( 'Product categories', 'woocommerce' ),
+				'label' => __( 'Title', 'woocommerce' ),
+			),
+			'orderby'            => array(
+				'type'    => 'select',
+				'std'     => 'name',
+				'label'   => __( 'Order by', 'woocommerce' ),
+				'options' => array(
+					'order' => __( 'Category order', 'woocommerce' ),
+					'name'  => __( 'Name', 'woocommerce' ),
+				),
+			),
+			'dropdown'           => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Show as dropdown', 'woocommerce' ),
+			),
+			'count'              => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Show product counts', 'woocommerce' ),
+			),
+			'hierarchical'       => array(
+				'type'  => 'checkbox',
+				'std'   => 1,
+				'label' => __( 'Show hierarchy', 'woocommerce' ),
+			),
+			'show_children_only' => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Only show children of the current category', 'woocommerce' ),
+			),
+			'hide_empty'         => array(
+				'type'  => 'checkbox',
+				'std'   => 0,
+				'label' => __( 'Hide empty categories', 'woocommerce' ),
+			),
+			'max_depth'          => array(
+				'type'  => 'text',
+				'std'   => '',
+				'label' => __( 'Maximum depth', 'woocommerce' ),
+			),
+		);
+
 		parent::__construct();
 	}
 	/**
