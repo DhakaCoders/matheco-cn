5b45a7643d230a8aadb3ac83e761d384afc9b6de
haque <azizul.softsw@gmail.com>
2021-05-04 19:54:43 +0600

    checkout

--- themes/matheco/woocommerce/checkout/form-checkout.php
+++ themes/matheco/woocommerce/checkout/form-checkout.php
@@ -82,10 +82,30 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 								<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 							</div>
 							<div class="woocommerce-additional-fields extra-info">	
-								<h3>4. <?php esc_html_e( 'Extra Info', 'woocommerce' ); ?></h3>
-								<div class="woocommerce-additional-fields__field-wrapper">
+								<!-- <h3>4. <?php esc_html_e( 'Extra Info', 'woocommerce' ); ?></h3> -->
+								<!-- <div class="woocommerce-additional-fields__field-wrapper">
 									<p class="form-row notes thwcfd-field-wrapper thwcfd-field-textarea" id="order_comments_field" ><span class="woocommerce-input-wrapper"><textarea name="order_comments" class="input-text " id="order_comments" placeholder="" rows="2" cols="5"></textarea></span></p>	
-								</div>
+								</div> -->
+
+							<div class="woocommerce-additional-fields">
+								<?php do_action( 'woocommerce_before_order_notes', $checkout ); ?>
+
+								<?php if ( apply_filters( 'woocommerce_enable_order_notes_field', 'yes' === get_option( 'woocommerce_enable_order_comments', 'yes' ) ) ) : ?>
+
+									<h3>4. <?php esc_html_e( 'Extra Info', 'woocommerce' ); ?></h3>
+
+									<div class="woocommerce-additional-fields__field-wrapper">
+										<?php foreach ( $checkout->get_checkout_fields( 'order' ) as $key => $field ) : ?>
+											<?php woocommerce_form_field( $key, $field, $checkout->get_value( $key ) ); ?>
+										<?php endforeach; ?>
+									</div>
+
+								<?php endif; ?>
+
+								<?php do_action( 'woocommerce_after_order_notes', $checkout ); ?>
+							</div>
+
+							
 							</div>
 							<div class="custom-checkout-btn">
 								<button type="submit" class="button alt" name="woocommerce_checkout_place_order" value="Afrekenen" data-value="Afrekenen"><?php esc_html_e( 'Afrekenen', 'woocommerce' ); ?></button>
